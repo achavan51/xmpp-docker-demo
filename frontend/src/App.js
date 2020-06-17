bf72f87a7a7a69b77551ad1e386089ec8d0b71a9
ganeshvarhadethink <ganesh.varhade.think@gmail.com>
2020-06-17 15:19:06 +0530

    add ui and apis

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PostForm />
+        {/* <PostList /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
